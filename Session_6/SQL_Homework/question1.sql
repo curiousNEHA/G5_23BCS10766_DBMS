259318dfd4e46b1c4f8ff5f397e5d36431f8b07a
Neha Sharma <155069649+curiousNEHA@users.noreply.github.com>
2025-12-11 22:12:36 +0530

    Update question1.sql

--- Session_6/SQL_Homework/question1.sql
+++ Session_6/SQL_Homework/question1.sql
@@ -1,1 +1,21 @@
+select name as results
+from (
+  select u.name, count(*) as cnt
+  from users u
+  join movierating mr on u.user_id = mr.user_id
+  group by u.name
+  order by cnt desc, u.name asc
+  limit 1
+) t
+union all
+select title as results
+from (
+  select m.title, avg(mr.rating) as avg_r
+  from movies m
+  join movierating mr on m.movie_id = mr.movie_id
+  where mr.created_at between '2020-02-01' and '2020-02-29'
+  group by m.title
+  order by avg_r desc, m.title asc
+  limit 1
+) s;
 
