ffc1786a53c6303e6c37f6ff58f20e796ec07e31
Neha Sharma <155069649+curiousNEHA@users.noreply.github.com>
2025-12-11 20:13:44 +0530

    Update question2.sql

--- Session_2/SQL_Questions/question2.sql
+++ Session_2/SQL_Questions/question2.sql
@@ -1,1 +1,10 @@
+select machine_id,round((td.en-td.st)/td.cn ,3)as processing_time
+from(
+select machine_id,sum(case when activity_type = 'start' then timestamp else 0 end) as st,
+sum(case when activity_type = 'end' then timestamp else 0 end) as en,count( distinct process_id) as cn
+from Activity
+group by machine_id
+) as td
+
+
 
