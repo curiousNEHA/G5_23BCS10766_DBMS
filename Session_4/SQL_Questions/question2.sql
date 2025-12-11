949b69444b8e2630182241c5d8eec582bc739041
Neha Sharma <155069649+curiousNEHA@users.noreply.github.com>
2025-12-11 21:31:28 +0530

    Create question2.sql

---
+++ Session_4/SQL_Questions/question2.sql
@@ -0,0 +1,35 @@
+CREATE TABLE Student (
+    student_id INT PRIMARY KEY,
+    student_name VARCHAR(50)
+);
+
+
+drop table Enrollments
+CREATE TABLE Enrollments (
+    student_id INT,
+    course VARCHAR(50)
+);
+
+
+INSERT INTO Student (student_id, student_name) VALUES
+(1, 'Alice'),
+(2, 'Bob'),
+(3, 'Charlie'),
+(4, 'David');
+
+INSERT INTO Enrollments (student_id, course) VALUES
+(1, 'DBMS'),
+(3, 'Java'),
+(5, 'Python');
+
+
+select s1.student_id,s1.student_name
+from
+Student s1
+left join
+Enrollments s2
+on s1.student_id = s2.student_id
+where s2.student_id is null
+
+
+
