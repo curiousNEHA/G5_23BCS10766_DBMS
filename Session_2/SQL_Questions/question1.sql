cffec4e48bc64e51eb3b56876947b2b7a6d8a91b
Neha Sharma <155069649+curiousNEHA@users.noreply.github.com>
2025-12-11 20:01:56 +0530

    Update question1.sql

--- Session_2/SQL_Questions/question1.sql
+++ Session_2/SQL_Questions/question1.sql
@@ -1,1 +1,19 @@
+CREATE TABLE person (
+    id INT PRIMARY KEY,
+    email VARCHAR(100)
+);
+
+INSERT INTO person (id, email) VALUES
+(1, 'john@example.com'),
+(2, 'bob@example.com'),
+(3, 'john@example.com');
+
+delete from person
+where id in
+(select p1.id
+from
+person p1
+join
+person p2
+on p1.id > p2.id and p1.email = p2.email)
 
