6e04cc0a76561fa9060af84511e9eba3abc85244
Neha Sharma <155069649+curiousNEHA@users.noreply.github.com>
2025-12-11 20:19:54 +0530

    Update question1.sql

--- Session_2/SQL_Homework/question1.sql
+++ Session_2/SQL_Homework/question1.sql
@@ -1,1 +1,12 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+RETURN (
+ select salary from 
+(select id, salary,
+DENSE_RANK() OVER (ORDER BY salary DESC) AS dr
+FROM Employee) as t
+where dr = N
+limit 1
+);
+END
 
